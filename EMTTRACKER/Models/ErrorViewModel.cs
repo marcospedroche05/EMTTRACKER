0cfd1606242c79f50001591bb2b5bf3f5480529c
marcospedroche05 <marcospedroche62@gmail.com>
2026-02-18 19:07:02 +0100

    Add project files.

---
+++ EMTTRACKER/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EMTTRACKER.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
